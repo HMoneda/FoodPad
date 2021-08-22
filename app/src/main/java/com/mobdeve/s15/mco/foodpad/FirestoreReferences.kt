7c7fe758d92733a511ec8832d20354e0500dcca8
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-08-22 18:23:04 +0800

    Added Validation for RegisterActivity

---
+++ app/src/main/java/com/mobdeve/s15/mco/foodpad/FirestoreReferences.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s15.mco.foodpad
+
+class FirestoreReferences {
+    companion object{
+        const val USERS_COLLECTION = "users"
+        const val RECIPES_COLLECTION = "recipes"
+
+        const val EMAIL_FIELD = "email"
+        const val USERNAME_FIELD = "username"
+    }
+}
--- app/src/main/java/com/mobdeve/s15/mco/foodpad/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/s15/mco/foodpad/RegisterActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -22,7 +25,8 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var signupBtn : Button
     private lateinit var loginTv : TextView
 
-    private var TAG = LogTags.REGISTER_ACTIVITY.name
+    private val TAG = LogTags.REGISTER_ACTIVITY.name
+    private val PASSWORD_LENGTH = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,31 +42,50 @@ class RegisterActivity : AppCompatActivity() {
         loginTv = findViewById(R.id.loginTV)
 
         signupBtn.setOnClickListener {
-            var username : String = usernameEt.text.toString()
-            var email : String = emailEt.text.toString()
-            var password : String = passwordEt.text.toString()
+            signupUser()
+        }
 
-            var newUser = User(username, email, "", 0, ArrayList())
+        loginTv.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
-            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
-                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_LONG).show()
-            }else{
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                    if(task.isSuccessful){
-                        db.collection("users").add(newUser).addOnSuccessListener {
-                            Log.d(TAG, "DocumentSnapshot added with ID: ${it.id}")
-                        }.addOnFailureListener{
-                            Log.w(LogTags.LOGIN_ACTIVITY.name, "Error Adding Document", it)
-                        }
-                        val i = Intent(this, HomeActivity::class.java)
-                        startActivity(i)
-                        finish()
+    private fun signupUser(){
+        val username : String = usernameEt.text.toString()
+        val email : String = emailEt.text.toString()
+        val password : String = passwordEt.text.toString()
+
+        val newUser = User(username, email, "", 0, ArrayList())
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please fill all the fields!", Toast.LENGTH_LONG).show()
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Please provide valid email!", Toast.LENGTH_LONG).show()
+        }else if(password.length < PASSWORD_LENGTH){
+            Toast.makeText(this, "Password must be 8 characters long!", Toast.LENGTH_LONG).show()
+        } else{
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    db.collection(FirestoreReferences.USERS_COLLECTION).add(newUser).addOnSuccessListener {
+                        Log.d(TAG, "DocumentSnapshot added with ID: ${it.id}")
+                    }.addOnFailureListener{
+                        Log.w(LogTags.LOGIN_ACTIVITY.name, "Error Adding Document", it)
+                    }
+                    val i = Intent(this, HomeActivity::class.java)
+                    startActivity(i)
+                    finish()
+                }else{
+                    Log.w(TAG, task.exception)
+                    if(task.exception is FirebaseAuthUserCollisionException){
+                        Toast.makeText(this, "Email Already Registered", Toast.LENGTH_LONG).show()
                     }else{
                         Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
                     }
                 }
             }
         }
-
     }
+
 }
